925f9b410dc317839fbb23963f71cb634ae0095a
SaschaRunge <sascharunge89@yahoo.de>
2026-02-19 11:50:24 +0100

    implementation of basic command interface

--- commands.go
+++ commands.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 )
@@ -12,8 +11,46 @@ type cliCommand struct {
 	callback    func() error
 }
 
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Displays names of locations in the Pokemon world. Each call shows 20 locations",
+			callback:    commandMap,
+		},
+	}
+}
+
 func commandExit() error {
-	fmt.Printf("Closing the Pokedex... Goodbye!")
+	fmt.Printf("Closing the Pokedex... Goodbye!\n")
 	os.Exit(0)
-	return errors.New("Couldn't exit.")
+
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\n")
+	fmt.Printf("Usage:\n\n")
+
+	commands := getCommands()
+
+	for _, command := range commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+
+	return nil
+}
+
+func commandMap() error {
+	return nil
 }
--- main.go
+++ main.go
@@ -9,11 +9,22 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	input := []string{}
+	commands := getCommands()
 
+	commands["help"].callback()
 	for true {
 		fmt.Print("Pokedex >")
 		scanner.Scan()
 		input = cleanInput(scanner.Text())
-		//fmt.Printf("Your command was: %s\n", input[0])
+		if command, ok := commands[input[0]]; ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Printf("Callback returned error: %v\n", err)
+			}
+
+		} else {
+			fmt.Printf("Unknown command")
+		}
 	}
+
 }

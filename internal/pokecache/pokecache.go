050026d24d1f8af35e905e88877c875e81b6a709
SaschaRunge <sascharunge89@yahoo.de>
2026-03-12 13:56:00 +0100

    finalized reapLoop in Cache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -20,6 +20,9 @@ func NewCache(interval time.Duration) *Cache {
 	c := &Cache{
 		entries: make(map[string]cacheEntry),
 	}
+
+	go c.reapLoop(interval)
+
 	return c
 }
 
@@ -42,21 +45,22 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 }
 
 func (c *Cache) reapLoop(interval time.Duration) {
-
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
 
 	for t := range ticker.C {
-		fmt.Printf("ticker at: %v\n", t)
-		c.mu.Lock()
-		for key, entry := range c.entries {
-			if t.Sub(entry.createdAt) >= interval {
-				delete(c.entries, key)
-			}
-		}
-		c.mu.Unlock()
+		fmt.Printf("ticker at: %v\n", t) //TODO: drop t once confirmed working
+		c.reap(interval)
 	}
 }
 
 func (c *Cache) reap(interval time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for key, entry := range c.entries {
+		if time.Since(entry.createdAt) >= interval {
+			delete(c.entries, key)
+		}
+	}
 }
